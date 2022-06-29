65a0131b3bf7e4860885d4ad715e5950b6dcea45
sdoisne <mrsylvain041>
2022-06-29 09:54:12 +0200

    Versioning du 29/06/2022 : correction d'un problème lié au cryptage du mot de passe. Application fonctionnelle et comptes utilisateurs sécurisés.

--- beerboard - edu/src/main/java/fr/almeri/beerboard/controllers/ProfileController.java
+++ beerboard - edu/src/main/java/fr/almeri/beerboard/controllers/ProfileController.java
@@ -1,9 +1,4 @@
 package fr.almeri.beerboard.controllers;
-
-//import fr.almeri.beerboard.Utils;
-import fr.almeri.beerboard.models.BiereId;
-import fr.almeri.beerboard.models.Brasserie;
-import fr.almeri.beerboard.models.Region;
 import fr.almeri.beerboard.models.Utilisateur;
 import fr.almeri.beerboard.repositories.UtilisateurRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,11 +11,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpSession;
-import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.NoSuchProviderException;
-import java.security.SecureRandom;
-import java.util.ArrayList;
 
 @Controller
 public class ProfileController {
@@ -34,41 +26,24 @@ public class ProfileController {
     }
 
     @PostMapping("/connect-user")
-    public String connectUtilisateur(@ModelAttribute Utilisateur utilisateur, @RequestParam String emailUtilisateur, RedirectAttributes redir, HttpSession session) {
-        Utilisateur userTest = utilisateurRepository.compteExistant(emailUtilisateur);
-        if (userTest != null) {
-            if (utilisateur.getMdpUtilisateur().equals(userTest.getMdpUtilisateur())) {
-                session.setAttribute("infoConnexion",userTest);
-                return "redirect:/index";
-            }
-        }
-        redir.addFlashAttribute("msg", "Le login et/ou le mot de passe est/sont erronés.");
-        return "redirect:/";
-    }
+    public String connectUtilisateur(@ModelAttribute Utilisateur utilisateur, HttpSession session, Model model) throws NoSuchAlgorithmException, NoSuchFieldException, NoSuchProviderException {
 
-//    @PostMapping("/connect-user")
-//    public String connectUtilisateur(@ModelAttribute Utilisateur utilisateur, HttpSession session, Model model) throws NoSuchAlgorithmException, NoSuchFieldException, NoSuchProviderException {
-//
-//        // Vérification au niveau de la base de données.
-//        boolean isOk = this.checkPassword(utilisateur);
-//        if (isOk)
-//        {
-//            // Hachage de mot de passe
-//            utilisateur.setSalt(Utils.getSalt());
-//            utilisateur.setMdpUtilisateur(Utils.hashMD5withSalt(utilisateur.getMdpUtilisateur(), utilisateur.getSalt()));
-//            utilisateurRepository.save(utilisateur);
-//            // Démarre une session
-//            // Création d'une session avec
-//            // utilisateur connecté
-//            session.setAttribute("infoConnexion", utilisateur);
-//            session.setMaxInactiveInterval(60);
-//            // Si connexion Ok
-//            return "redirect:/index";
-//        } else {
-//            model.addAttribute("isBad", true);
-//            return "redirect:/";
-//        }
-//    }
+        // Vérification au niveau de la base de données.
+        boolean isOk = this.checkPassword(utilisateur);
+        if (isOk) {
+            // Démarre une session
+            // Création d'une session avec
+            // utilisateur connecté
+            utilisateur = utilisateurRepository.compteExistant(utilisateur.getEmailUtilisateur());
+            session.setAttribute("infoConnexion", utilisateur);
+            session.setMaxInactiveInterval(60);
+            // Si connexion Ok
+            return "redirect:/index";
+        } else {
+            model.addAttribute("isBad", true);
+            return "redirect:/";
+        }
+    }
 
     @GetMapping("/motdepasse")
     public String motdepasse(Model pModel) {
@@ -94,12 +69,16 @@ public class ProfileController {
     }
 
     @PostMapping("/valid-user")
-    public String addNouvelUtilisateur(@ModelAttribute Utilisateur utilisateur, @RequestParam String emailUtilisateur, @RequestParam String inputPasswordConfirm, @RequestParam String mdpUtilisateur, Model pModel, RedirectAttributes redir) {
+    public String addNouvelUtilisateur(@ModelAttribute Utilisateur utilisateur, @RequestParam String emailUtilisateur, @RequestParam String inputPasswordConfirm, @RequestParam String mdpUtilisateur, Model pModel, RedirectAttributes redir) throws NoSuchAlgorithmException, NoSuchFieldException, NoSuchProviderException {
         if (inputPasswordConfirm.equals(mdpUtilisateur)) {
             if (utilisateurRepository.emailExistant(emailUtilisateur) != null) {
                 redir.addFlashAttribute("msg", "Email déjà existant. Veuillez vous connecter en utilisant cet email.");
                 return "redirect:/register";
             } else {
+                byte[] salt = UserController.getSalt();
+                String password = UserController.hashMD5withSalt(utilisateur.getMdpUtilisateur(), salt);
+                utilisateur.setMdpUtilisateur(password);
+                utilisateur.setSalt(salt);
                 utilisateurRepository.save(utilisateur);
                 redir.addFlashAttribute("msg", "Le compte a bien été créé. Veuillez vous connecter.");
                 return "redirect:/";
@@ -109,21 +88,18 @@ public class ProfileController {
             return "redirect:/register";
         }
     }
-//    private boolean checkPassword(Utilisateur utilisateur) throws NoSuchAlgorithmException, NoSuchFieldException, NoSuchProviderException {
-//        // On cherche le user en BDD à l'aide de son login
-//        Utilisateur u = utilisateurRepository.compteExistant(utilisateur.getEmailUtilisateur());
-//
-//        // Si mot de passe ne correspond pas
-//        if (u == null)
-//        {
-//            return false;
-//        }
-//
-//        // Chiffrage du mot de passe saisi par l'utilisateur
-//        // Pour comparaison
-//        String newPass = Utils.hashMD5withSalt(utilisateur.getMdpUtilisateur(), u.getSalt());
-//
-//        // Retourne True si mot de passe saisi == mot de passe de la base de données
-//        return newPass.equals(u.getMdpUtilisateur());
-//    }
+    private boolean checkPassword(Utilisateur utilisateur) throws NoSuchAlgorithmException, NoSuchFieldException, NoSuchProviderException {
+        // On cherche le user en BDD à l'aide de son login
+        Utilisateur u = utilisateurRepository.compteExistant(utilisateur.getEmailUtilisateur());
+        // Si mot de passe ne correspond pas
+        if (u == null)
+        {
+            return false;
+        }
+        // Chiffrage du mot de passe saisi par l'utilisateur
+        // Pour comparaison
+        String newPass = UserController.hashMD5withSalt(utilisateur.getMdpUtilisateur(), u.getSalt());
+        // Retourne True si mot de passe saisi == mot de passe de la base de données
+        return newPass.equals(u.getMdpUtilisateur());
+    }
 }
---
+++ beerboard - edu/src/main/java/fr/almeri/beerboard/controllers/UserController.java
@@ -0,0 +1,45 @@
+package fr.almeri.beerboard.controllers;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.SecureRandom;
+
+public class UserController {
+    public static String hashMD5withSalt(String passwordToHash, byte[] salt) {
+        String generatedPassword = null;
+
+        MessageDigest md = null;
+
+        try {
+            md = MessageDigest.getInstance("MD5");
+
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+
+        md.update(salt);
+
+        byte[] bytes = md.digest(passwordToHash.getBytes());
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < bytes.length; i++) {
+            sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16)).substring(1);
+        }
+
+        generatedPassword = sb.toString();
+
+        return generatedPassword;
+    }
+
+    public static byte[] getSalt() throws NoSuchFieldException, NoSuchFieldException, NoSuchAlgorithmException, NoSuchProviderException {
+        SecureRandom sr = SecureRandom.getInstance("SHA1PRNG", "SUN");
+
+        byte[] salt = new byte[16];
+
+        sr.nextBytes(salt);
+
+        return salt;
+    }
+}
